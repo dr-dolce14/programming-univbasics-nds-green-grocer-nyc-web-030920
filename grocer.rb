fbd277d45444463d19cb7dd6793096d97456ff63
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-07 16:41:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,13 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  thing = 0
+     while thing < collection.length do
+      if name == collection[thing][:item]
+        return collection[thing]
+      end
+      thing +=1
+    end
 end
 
 def consolidate_cart(cart)
@@ -9,18 +16,56 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+   consolidated_cart = []
+    thing = 0
+    while thing < cart.length do
+      if (find_item_by_name_in_collection(cart[thing][:item], consolidated_cart) == nil)
+        cart[thing][:count] = 1
+        consolidated_cart << cart[thing]
+      else
+        find_item_by_name_in_collection(cart[thing][:item], cart)[:count] +=1
+      end
+      thing +=1
+    end
+    consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+   coupon_index = 0
+  while coupon_index < coupons.length do
+    coupon_item_present = find_item_by_name_in_collection(coupons[coupon_index][:item], cart) 
+    if ( coupon_item_present[:count] / coupons[coupon_index][:num] >= 1 )
+      cart.push ( {:item => "#{coupons[coupon_index][:item]} W/COUPON",
+               :price => (coupons[coupon_index][:cost] / 
+              coupons[coupon_index][:num].round(2)),
+              :clearance => coupon_item_present[:clearance],
+              :count => coupon_item_present[:count] - (coupon_item_present[:count] % coupons[coupon_index][:num])
+             })
+
+             coupon_item_present[:count] %= coupons[coupon_index][:num]
+            end
+            coupon_index +=1
+          end
+          cart
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+   cart_with_clearance = []
+  cart_index = 0
+  while cart_index < cart.length do
+    if cart[cart_index][:clearance]
+      cart[cart_index][:price] = cart[cart_index][:price] - (cart[cart_index][:price] * 0.20)
+    end
+    cart_with_clearance << cart[cart_index]
+    cart_index +=1
+  end
+  cart_with_clearance
 end
 
 def checkout(cart, coupons)
